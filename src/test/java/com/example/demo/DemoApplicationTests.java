b6efe6d66f2ed75795eac4dd868f58c8e2dc6656
Relativiteit <alejocain@gmail.com>
2021-09-03 14:12:40 +0200

    Learning Testing

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -22,11 +22,15 @@ class DemoApplicationTests {
 		int expected = 50;
 		assertThat(result).isEqualTo(expected);
 	}
-	class Calculator{
-		int add(int a, int b) {return a + b;}
-
+	class Calculator {
+		int add(int a, int b) {
+			return a + b;
+		}
 
 	}
 }
+
+
+
 
 
--- src/test/java/com/example/demo/student/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -1,5 +1,32 @@
 package com.example.demo.student;
-// test for StudentRepository
 
-public class StudentRepositoryTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+class StudentRepositoryTest {
+    @Autowired
+    private StudentRepository underTest;
+
+    @Test
+    void itShouldCheckIfStudentExistsEmail() {
+
+        //given
+        String email = "jamila@gmail.com";
+        Student student = new Student
+                ("Jamila",
+                        email,
+                        Gender.FEMALE);
+        underTest.save(student);
+
+        //when
+        boolean expected = underTest.selectExistsEmail(email);
+
+        //then
+        assertThat(expected).isTrue();
+
+    }
 }
