fca91ce00adb7f99461237982119c3cb04da184b
Relativiteit <alejocain@gmail.com>
2021-09-03 15:14:19 +0200

    CanAddStudent

---
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -0,0 +1,63 @@
+package com.example.demo.student;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class StudentServiceTest {
+
+
+    @Mock private StudentRepository studentRepository;
+    private StudentService underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new StudentService(studentRepository);
+    }
+
+    @Test
+    void canGetAllStudents() {
+        //when
+        underTest.getAllStudents();
+        //then
+        verify(studentRepository).findAll();
+
+    }
+
+    @Test
+    void CanAddStudent() {
+        //given
+        String email = "jamila@gmail.com";
+        Student student = new Student
+                ("Jamila",
+                        email,
+                        Gender.FEMALE);
+
+        //when
+        underTest.addStudent(student);
+        //then
+        ArgumentCaptor<Student> studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
+
+        verify(studentRepository).save(studentArgumentCaptor.capture());
+
+        Student capturedStudent = studentArgumentCaptor.getValue();
+
+        assertThat(capturedStudent).isEqualTo(student);
+
+    }
+
+    @Test
+    @Disabled
+    void deleteStudent() {
+    }
+}
